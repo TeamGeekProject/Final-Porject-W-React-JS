b29b5ac8e38c617d8eb992e988437b63648e9efa
Miguel Torres <mtorresroadby@gmail.com>
2023-05-01 08:57:44 -0400

    changing main structure

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,13 +6,13 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-dark bg-dark mb-3">
       <Link className="navbar-brand mx-3" to="/home">
-      <i className="fa fa-star text-danger" />
-       <span className="navbar-brand mb-0  ">Stars Music</span>
+        <i className="fa fa-star text-danger" />
+        <span className="navbar-brand mb-0  ">Stars Music</span>
       </Link>
       <div className="ml-auto">
-        <Link to="/contact" >
+        <Link to="/collection">
           <button className="btn btn-primary mx-3 itemFilter">
-          Soundtracks
+            Soundtracks
           </button>
         </Link>
       </div>
