a39e73a9a8efa1403b739d2a83246629be6ebc79
Miguel Torres <mtorresroadby@gmail.com>
2023-05-02 22:25:00 -0400

    fix

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-dark bg-dark mb-3">
-      <Link className="navbar-brand mx-3" to="/home">
+      <Link className="navbar-brand mx-2" to="/home">
         <i className="fa fa-star text-danger" />
         <span className="navbar-brand mb-0  ">Stars Music</span>
       </Link>
