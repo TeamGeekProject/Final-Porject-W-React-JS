7651d65b375ada2623ff180d3c6dda09bdca1f71
Johanna Castillo <122300795+joaarg86@users.noreply.github.com>
2023-05-03 22:20:42 -0400

    johanna

--- src/js/views/ipod.js
+++ src/js/views/ipod.js
@@ -1,105 +1,103 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import { useRef } from 'react';
-import ReactAudioPlayer from 'react-audio-player';
-import useSound from 'use-sound';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faForward , faBackward} from '@fortawesome/free-solid-svg-icons';
-import { faPauseCircle, faPlayCircle } from '@fortawesome/free-regular-svg-icons';
+import React from "react";
+import { useState, useEffect } from "react";
+import { useRef } from "react";
+import ReactAudioPlayer from "react-audio-player";
+// import useSound from 'use-sound';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faForward, faBackward } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPauseCircle,
+  faPlayCircle,
+} from "@fortawesome/free-regular-svg-icons";
 
-
-export const Ipod=()=> {
+export const Ipod = () => {
   const [currentMusicDetails, setCurrentMusicDetails] = useState({
-    songName: 'Chasing',
-    songArtist: 'NEFFEX',
-    songSrc: './Assets/songs/Chasing - NEFFEX.mp3'
-  })
+    songName: "Chasing",
+    songArtist: "NEFFEX",
+    songSrc: "./Assets/songs/Chasing - NEFFEX.mp3",
+  });
 
   //UseStates Variables
   const [audioProgress, setAudioProgress] = useState(0);
   const [isAudioPlaying, setIsAudioPlaying] = useState(false);
   const [musicIndex, setMusicIndex] = useState(0);
-  const [musicTotalLength, setMusicTotalLength] = useState('04 : 38');
-  const [musicCurrentTime, setMusicCurrentTime] = useState('00 : 00');
-  const [videoIndex, setVideoIndex] = useState(0)
+  const [musicTotalLength, setMusicTotalLength] = useState("04 : 38");
+  const [musicCurrentTime, setMusicCurrentTime] = useState("00 : 00");
+  const [videoIndex, setVideoIndex] = useState(0);
 
-  const currentAudio = useRef()
+  const currentAudio = useRef();
 
-  const handleMusicProgressBar = (e)=>{
+  const handleMusicProgressBar = (e) => {
     setAudioProgress(e.target.value);
-    currentAudio.current.currentTime = e.target.value * currentAudio.current.duration / 100;
-  }
-
+    currentAudio.current.currentTime =
+      (e.target.value * currentAudio.current.duration) / 100;
+  };
 
   //Play Audio Function
-  const handleAudioPlay = ()=>{
+  const handleAudioPlay = () => {
     if (currentAudio.current.paused) {
       currentAudio.current.play();
-      setIsAudioPlaying(true)
-    }else{
+      setIsAudioPlaying(true);
+    } else {
       currentAudio.current.pause();
-      setIsAudioPlaying(false)
+      setIsAudioPlaying(false);
     }
-  }
+  };
 
   const musicAPI = [
     {
-      songName: 'Chasing',
-      songArtist: 'NEFFEX',
-      songSrc: '../Apna Bana Le - Full Audio _ Bhediya _ Varun Dhawan',
+      songName: "Chasing",
+      songArtist: "NEFFEX",
+      songSrc: "../Apna Bana Le - Full Audio _ Bhediya _ Varun Dhawan",
+    },
+    {
+      songName: "AURORA - Runaway",
+      songArtist: "Aurora Aksnes",
+      songSrc: "../public_Assets_songs_AURORA - Runaway (Lyrics).mp3",
+    },
+    {
+      songName: "Catch Me If I Fall",
+      songArtist: "TEGNENT",
+      songSrc: "../public_Assets_songs_Catch Me If I Fall - NEFFEX.mp3",
+    },
+    {
+      songName: "Inspired (Clean)",
+      songArtist: "NEFFEX",
+      songSrc: "../public_Assets_songs_Inspired (Clean) - NEFFEX.mp3",
+    },
+    {
+      songName: "Baby doll [ slowed + reverb ]",
+      songArtist: "Kanika Kapoor",
+      songSrc:
+        "../public_Assets_songs_Baby doll [ slowed + reverb ] __ meet bros",
+    },
+  ];
 
-    },
-    {
-      songName: 'AURORA - Runaway',
-      songArtist: 'Aurora Aksnes',
-      songSrc: '../public_Assets_songs_AURORA - Runaway (Lyrics).mp3',
-    
-    },
-    {
-      songName: 'Catch Me If I Fall',
-      songArtist: 'TEGNENT',
-      songSrc: '../public_Assets_songs_Catch Me If I Fall - NEFFEX.mp3',
-    
-    },
-    {
-      songName: 'Inspired (Clean)',
-      songArtist: 'NEFFEX',
-      songSrc: '../public_Assets_songs_Inspired (Clean) - NEFFEX.mp3',
-    
-    },
-    {
-      songName: 'Baby doll [ slowed + reverb ]',
-      songArtist: 'Kanika Kapoor',
-      songSrc: '../public_Assets_songs_Baby doll [ slowed + reverb ] __ meet bros',
-    
-    },
-  ]
-
-  const handleNextSong = ()=>{
+  const handleNextSong = () => {
     if (musicIndex >= musicAPI.length - 1) {
       let setNumber = 0;
       setMusicIndex(setNumber);
       updateCurrentMusicDetails(setNumber);
-    }else{
+    } else {
       let setNumber = musicIndex + 1;
-      setMusicIndex(setNumber)
+      setMusicIndex(setNumber);
       updateCurrentMusicDetails(setNumber);
     }
-  }
+  };
 
-  const handlePrevSong = ()=>{
+  const handlePrevSong = () => {
     if (musicIndex === 0) {
       let setNumber = musicAPI.length - 1;
       setMusicIndex(setNumber);
       updateCurrentMusicDetails(setNumber);
-    }else{
+    } else {
       let setNumber = musicIndex - 1;
-      setMusicIndex(setNumber)
+      setMusicIndex(setNumber);
       updateCurrentMusicDetails(setNumber);
     }
-  }
+  };
 
-  const updateCurrentMusicDetails = (number)=>{
+  const updateCurrentMusicDetails = (number) => {
     let musicObject = musicAPI[number];
     currentAudio.current.src = musicObject.songSrc;
     currentAudio.current.play();
@@ -107,167 +105,90 @@ export const Ipod=()=> {
       songName: musicObject.songName,
       songArtist: musicObject.songArtist,
       songSrc: musicObject.songSrc,
-      
-    })
+    });
     setIsAudioPlaying(true);
-  }
+  };
 
-  const handleAudioUpdate = ()=>{
+  const handleAudioUpdate = () => {
     //Input total length of the audio
     let minutes = Math.floor(currentAudio.current.duration / 60);
     let seconds = Math.floor(currentAudio.current.duration % 60);
-    let musicTotalLength0 = `${minutes <10 ? `0${minutes}` : minutes} : ${seconds <10 ? `0${seconds}` : seconds}`;
+    let musicTotalLength0 = `${minutes < 10 ? `0${minutes}` : minutes} : ${
+      seconds < 10 ? `0${seconds}` : seconds
+    }`;
     setMusicTotalLength(musicTotalLength0);
 
     //Input Music Current Time
     let currentMin = Math.floor(currentAudio.current.currentTime / 60);
     let currentSec = Math.floor(currentAudio.current.currentTime % 60);
-    let musicCurrentT = `${currentMin <10 ? `0${currentMin}` : currentMin} : ${currentSec <10 ? `0${currentSec}` : currentSec}`;
+    let musicCurrentT = `${currentMin < 10 ? `0${currentMin}` : currentMin} : ${
+      currentSec < 10 ? `0${currentSec}` : currentSec
+    }`;
     setMusicCurrentTime(musicCurrentT);
 
-    const progress = parseInt((currentAudio.current.currentTime / currentAudio.current.duration) * 100);
-    setAudioProgress(isNaN(progress)? 0 : progress)
-  }
-
+    const progress = parseInt(
+      (currentAudio.current.currentTime / currentAudio.current.duration) * 100
+    );
+    setAudioProgress(isNaN(progress) ? 0 : progress);
+  };
 
   const vidArray = [""];
 
-  const handleChangeBackground = ()=>{
+  const handleChangeBackground = () => {
     if (videoIndex >= vidArray.length - 1) {
       setVideoIndex(0);
-    }else{
-      setVideoIndex(videoIndex + 1)
-    }
-  }
-//another way
-const [isPlaying, setIsPlaying] = useState(false);
-  const [time, setTime] = useState({
-    min: "",
-    sec: ""
-  });
-  const [currTime, setCurrTime] = useState({
-    min: "",
-    sec: ""
-  });
-
-  const [seconds, setSeconds] = useState();
-
-  const [play, { pause, duration, sound }] = useSound();
-
-  useEffect(() => {
-    if (duration) {
-      const sec = duration / 1000;
-      const min = Math.floor(sec / 60);
-      const secRemain = Math.floor(sec % 60);
-      setTime({
-        min: min,
-        sec: secRemain
-      });
-    }
-  }, [isPlaying]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (sound) {
-        setSeconds(sound.seek([]));
-        const min = Math.floor(sound.seek([]) / 60);
-        const sec = Math.floor(sound.seek([]) % 60);
-        setCurrTime({
-          min,
-          sec
-        });
-      }
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [sound]);
-
-  const playingButton = () => {
-    if (isPlaying) {
-      pause();
-      setIsPlaying(false);
     } else {
-      play();
-      setIsPlaying(true);
+      setVideoIndex(videoIndex + 1);
     }
   };
-//end another way
 
   return (
     <>
-<div className="component">
-      <h2>Playing Now</h2>
-      <img className="musicCover" src="https://picsum.photos/200/200" />
-      <div>
-        <h3 className="title" src=" ">Rubaiyyan</h3>
-        <p className="subTitle">Qala</p>
-      </div>
-      <div>
-        <div className="time">
-          <p>
-            {currTime.min}:{currTime.sec}
+      <div className="ipod container">
+        <audio
+          src="../Apna Bana Le - Full Audio _ Bhediya _ Varun Dhawan"
+          ref={currentAudio}
+          onEnded={handleNextSong}
+          onTimeUpdate={handleAudioUpdate}
+        ></audio>
+        <div className="blackScreen"></div>
+        <div className="music-Container">
+          <p className="music Player">
+            <strong>Playing Now</strong>
           </p>
-          <p>
-            {time.min}:{time.sec}
-          </p>
-        </div>
-        <input
-          type="range"
-          min="0"
-          max={duration / 1000}
-          default="0"
-          value={seconds}
-          className="timeline"
-          onChange={(e) => {
-            sound.seek([e.target.value]);
-          }}
-        />
-      </div>
-      <div>
-        <button className="playButton">
-          <FontAwesomeIcon icon={faBackward}/>
-        </button>
-        {!isPlaying ? (
-          <button className="playButton" onClick={playingButton}>
-            <FontAwesomeIcon icon={faPlayCircle}/>
-          </button>
-        ) : (
-          <button className="playButton" onClick={playingButton}>
-            <FontAwesomeIcon icon={faPauseCircle}/>
-          </button>
-        )}
-        <button className="playButton">
-          <FontAwesomeIcon icon={faForward}/>
-        </button>
-      </div>
-    </div>
-
-
-    <div className="ipod container">
-      <audio src='../Apna Bana Le - Full Audio _ Bhediya _ Varun Dhawan' ref={currentAudio} onEnded={handleNextSong} onTimeUpdate={handleAudioUpdate} ></audio>
-      <div className="blackScreen"></div>
-      <div className="music-Container">
-        <p className='music Player'><strong>Playing Now</strong></p>
-        <img className="musicCover" src="https://picsum.photos/200/200" />
-        <p className='music-Head-Name'>{currentMusicDetails.songName}</p>
-        <p className='music-Artist-Name'>{currentMusicDetails.songArtist}</p>
-        <div className="musicTimerDiv">
-          <p className='musicCurrentTime'>{musicCurrentTime}</p>
-          <p className='musicTotalLenght'>{musicTotalLength}</p>
-        </div>
-        <input type="range" name="musicProgressBar" className='musicProgressBar' value={audioProgress} onChange={handleMusicProgressBar} />
-        <ReactAudioPlayer
-            autoPlay
-            controls
-            src=" "
-            crossOrigin="anonymous" 
+          <img className="musicCover" src="https://picsum.photos/200/200" />
+          <p className="music-Head-Name">{currentMusicDetails.songName}</p>
+          <p className="music-Artist-Name">{currentMusicDetails.songArtist}</p>
+          <div className="musicTimerDiv">
+            <p className="musicCurrentTime">{musicCurrentTime}</p>
+            <p className="musicTotalLenght">{musicTotalLength}</p>
+          </div>
+          <input
+            type="range"
+            name="musicProgressBar"
+            className="musicProgressBar"
+            value={audioProgress}
+            onChange={handleMusicProgressBar}
           />
-        <div className="music Controlers">
-          <i className='fa-solid fa-backward musicControler' onClick={handlePrevSong}></i>
-          <i className={`fa-solid ${isAudioPlaying? 'fa-pause-circle' : 'fa-circle-play'} playBtn`} onClick={handleAudioPlay}></i>
-          <i className='fa-solid fa-forward musicControler' onClick={handleNextSong}></i>
+          <ReactAudioPlayer autoPlay controls src=" " crossOrigin="anonymous" />
+          <div className="music Controlers">
+            <i
+              className="fa-solid fa-backward musicControler"
+              onClick={handlePrevSong}
+            ></i>
+            <i
+              className={`fa-solid ${
+                isAudioPlaying ? "fa-pause-circle" : "fa-circle-play"
+              } playBtn`}
+              onClick={handleAudioPlay}
+            ></i>
+            <i
+              className="fa-solid fa-forward musicControler"
+              onClick={handleNextSong}
+            ></i>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
-}
+};
