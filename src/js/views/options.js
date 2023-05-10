ecd0145dc1c2724fb360e8363c9df9ec14ee2ef5
Johanna Castillo <122300795+joaarg86@users.noreply.github.com>
2023-05-10 05:00:46 -0400

    johanna

--- src/js/views/options.js
+++ src/js/views/options.js
@@ -16,12 +16,27 @@ export const Options = (props) => {
     <div className="jumbotron">
       <h1>Stars Music Entertainment</h1>
       <br />
-
+      <h1>Bio/ Our Story</h1>
+      <p>
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
+        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
+        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
+        quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+        voluptas nulla pariatur?"
+      </p>
       {/* <Auth /> */}
       <div>
         <Link to="/collection">
           <span
-            className="btn btn-secondary btn-lg itemFilter"
+            className="btn btn-secondary btn-md itemFilter"
             href="#"
             role="button"
           >
@@ -33,7 +48,7 @@ export const Options = (props) => {
       <div>
         <Link to="/reach-out">
           <span
-            className="btn btn-secondary btn-lg itemFilter"
+            className="btn btn-secondary btn-md itemFilter"
             href="#"
             role="button"
           >
@@ -45,7 +60,7 @@ export const Options = (props) => {
       <div>
         <Link to="/who">
           <span
-            className="btn btn-secondary btn-lg itemFilter"
+            className="btn btn-secondary btn-md itemFilter"
             href="#"
             role="button "
           >
