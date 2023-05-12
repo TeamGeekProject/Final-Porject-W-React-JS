9caa7e3447a0a4a00928db079d9b62ac2e08120b
Johanna Castillo <122300795+joaarg86@users.noreply.github.com>
2023-05-12 01:30:02 -0400

    johanna

--- src/js/views/who.js
+++ src/js/views/who.js
@@ -7,18 +7,19 @@ import { CardImg } from "react-bootstrap";
 export const Who = () => (
   <div className="text-center mt-5">
     <div className="row justify-content-center">
-      <div className="col-4 container rounded">
+      <div className="col-6 container rounded pt-3 pb-3 mb-5">
       
         <h3 className="text-secondary">This project has been created by:</h3>
 
-        <div className="list-group ">
+        <div className="list-group">
         <a
             href="https://github.com/organizations/TeamGeekProject/settings/profile"
             className="list-group-item list-group-item-action itemFilter"
             aria-current="true"
           >
-          <img className="musicCover rounded" src="https://avatars.githubusercontent.com/u/128603767?s=400&u=4889c435b45beda27f7e669e0864d152aa4f5965&v=4" style={{width: '50px', height: '50px'}}/>
-          <br/>
+          <div className="image-wrapper pt-2">
+          <img className="circle-image" src="https://avatars.githubusercontent.com/u/128603767?s=400&u=4889c435b45beda27f7e669e0864d152aa4f5965&v=4" style={{width: '50px', height: '50px'}}/>
+          </div>
             TeamGeek
           </a>
 
@@ -27,24 +28,27 @@ export const Who = () => (
             className="list-group-item list-group-item-action itemFilter"
             aria-current="true"
           >
-          <img className="musicCover rounded" src="https://avatars.githubusercontent.com/u/103975556?v=4" style={{width: '50px', height: '50px'}}/>
-          <br/>
+          <div className="image-wrapper pt-2">
+          <img className=" circle-image" src="https://avatars.githubusercontent.com/u/103975556?v=4" style={{width: '50px', height: '50px'}}/>
+          </div>
             Balim1
           </a>
           <a
             href="https://github.com/joaarg86"
             className="list-group-item list-group-item-action itemFilter"
           >
-          <img className="musicCover rounded" src="https://avatars.githubusercontent.com/u/122300795?v=4" style={{width: '50px', height: '50px'}}/>
-          <br/>
+          <div className="image-wrapper pt-2">
+          <img className="circle-image" src="https://avatars.githubusercontent.com/u/122300795?v=4" style={{width: '50px', height: '50px'}}/>
+          </div>
             joaarg86
           </a>
           <a
             href="https://github.com/raulyfs11"
             className="list-group-item list-group-item-action itemFilter"
           >
-          <img className="musicCover rounded" src="https://avatars.githubusercontent.com/u/91436792?v=4" style={{width: '50px', height: '50px'}}/>
-          <br/>
+          <div className="image-wrapper pt-2">
+          <img className="circle-image" src="https://avatars.githubusercontent.com/u/91436792?v=4" style={{width: '50px', height: '50px'}}/>
+          </div>
             raulyfs11
           </a>
           <a
@@ -53,8 +57,9 @@ export const Who = () => (
             tabIndex="-1"
             aria-disabled="true"
           >
-          <img className="rounded" src="https://avatars.githubusercontent.com/u/81215009?v=4" style={{width: '50px', height: '50px'}}/>
-          <br/>
+          <div className="image-wrapper pt-2">
+          <img className="circle-image" src="https://avatars.githubusercontent.com/u/81215009?v=4" style={{width: '50px', height: '50px'}}/>
+         </div>
             spacemc2
           </a>
         </div>
